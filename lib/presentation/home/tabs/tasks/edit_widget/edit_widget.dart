da22b11f21a59f8d05978c2e7fdcb6753d828faa
mariam2002212 <mariam2002azima@gmail.com>
2024-11-09 03:58:32 +0200

    +login ui +register ui

---
+++ lib/presentation/home/tabs/tasks/edit_widget/edit_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EditWidget extends StatelessWidget {
+  const EditWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      color: Colors.orange,
+    );
+  }
+}
